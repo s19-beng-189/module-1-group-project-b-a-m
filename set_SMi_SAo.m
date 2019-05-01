b7f785e15f1b06cb01cb0940df37c99db5d66617
btierra <49373949+btierra@users.noreply.github.com>
2019-05-01 13:05:43 -0700

    Removed extra parameter

--- set_SMi_SAo.m
+++ set_SMi_SAo.m
@@ -6,7 +6,7 @@ while(~done)  %keep trying if not done (see below)
   SMi_noted=SMi;  %note the value of SMi
   SAo_noted=SAo;  %note the value of SAo
   % set pressures based on valve states:
-  [PLV,Psa]=PLV_Psa_new(PLV_old,Psa_old,CLV_old,CLV,SMi,SAo,klokmax);
+  [PLV,Psa]=PLV_Psa_new(PLV_old,Psa_old,CLV_old,CLV,SMi,SAo);
   %and then set valve states based on pressures:
   SMi=(PLA>PLV); %evaluates to 1 if PLA>PLV, 0 otherwise
   SAo=(PLV>Psa); %evaluates to 1 if PLV>Psa, 0 otherwise
